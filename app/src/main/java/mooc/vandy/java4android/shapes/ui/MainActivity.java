5b2ffb902c3c78beeb19e9d9269307dcb92e9256
Vidish Naik <vidishnaik0509@gmail.com>
2021-10-13 19:51:11 -0700

    bump project to latest gradle

--- app/src/main/java/mooc/vandy/java4android/shapes/ui/MainActivity.java
+++ app/src/main/java/mooc/vandy/java4android/shapes/ui/MainActivity.java
@@ -1,6 +1,6 @@
 package mooc.vandy.java4android.shapes.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
